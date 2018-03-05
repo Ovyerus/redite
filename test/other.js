99bd7f291f20ac8c2c369c0c4bb87ae35a5c260d
Ovyerus <iamovyerus@gmail.com>
2018-03-05 18:55:57 +1100

    Add coverage for custom inspection

--- test/other.js
+++ test/other.js
@@ -9,10 +9,11 @@ const {expect} = chai;
 
 const Redite = require('../');
 const redis = require('redis');
+const {inspect: {custom}} = require('util');
 const {fork} = require('child_process');
 
 const client = redis.createClient({db: DB});
-const wrapper = new Redite({client});
+const wrapper = new Redite({client, customInspection: true});
 
 // Clear out the database before and after use, to make sure that all data is just our own.
 beforeEach(function(done) {
@@ -27,9 +28,11 @@ describe('Extra coverage', function() {
     it('should auto-gen settings when not given anything', function() {
         let db = new Redite();
 
+        expect(db._redis).to.be.instanceof(redis.RedisClient);
         expect(db._serialise).to.equal(JSON.stringify);
         expect(db._parse).to.equal(JSON.parse);
         expect(db._deletedString).to.equal('@__DELETED__@');
+        expect(db._customInspection).to.be.false;
     });
 
     it('should unref if told to', function(done) {
@@ -97,6 +100,26 @@ describe('Extra coverage', function() {
                 expect(err).to.be.instanceof(Error);
                 expect(err.message).to.equal('At least one key is required in the stack');
             }
+        });
+    });
+
+    describe('util.inspect.custom (custom inspection)', function() {
+        it('should be a function', function() {
+            expect(wrapper[custom]).to.be.a('function');
+        });
+
+        it('should return a new class called `Redite`', function() {
+            expect(wrapper[custom]().constructor.name).to.equal('Redite');
+        });
+
+        it('should have regular values, with `redis` set to "<hidden>"', function() {
+            let res = wrapper[custom]();
+
+            expect(res.redis).to.equal('<hidden>');
+            expect(res.serialise).to.equal(wrapper._serialise);
+            expect(res.parse).to.equal(wrapper._parse);
+            expect(res.deletedString).to.equal(wrapper._deletedString);
+            expect(res.customInspection).to.equal(wrapper._customInspection);
         });
     });
 });
