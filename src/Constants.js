92f5e066fabb3ecdf40baa94f31cf6689a4cc6f6
Ovyerus <iamovyerus@gmail.com>
2018-10-11 18:00:56 +1100

    style: expand constant arrays to multiline

--- src/Constants.js
+++ src/Constants.js
@@ -1,5 +1,24 @@
-const MutatingMethods = ['push', 'remove', 'removeIndex', 'pop', 'shift', 'unshift'];
-const NonMutatingMethods = ['concat', 'find', 'findIndex', 'includes', 'indexOf', 'lastIndexOf', 'map', 'length', 'filter', 'join', 'forEach'];
+const MutatingMethods = [
+    'push',
+    'remove',
+    'removeIndex',
+    'pop',
+    'shift',
+    'unshift'
+];
+const NonMutatingMethods = [
+    'concat',
+    'find',
+    'findIndex',
+    'includes',
+    'indexOf',
+    'lastIndexOf',
+    'map',
+    'length',
+    'filter',
+    'join',
+    'forEach'
+];
 const SupportedArrayMethods = MutatingMethods.concat(NonMutatingMethods);
 
 module.exports = {
